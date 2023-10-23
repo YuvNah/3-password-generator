8fa03d96acd4e586aaf268dc92166c23fe586d58
Yuval Nahmias <yuvalit789@gmail.com>
2023-10-23 18:58:08 -0400

    added comments for easy review

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// the different chcracters to choose from for the password
 var letters = [
   "a",
   "b",
@@ -58,25 +60,30 @@ var uppercaseLetters = [
   "Z",
 ];
 
-console.log;
-
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function generatePassword() {
+  /* first critiria that has to be met to create a password - length
+  if it does not meet the criteria the user will start over */
   var numberOfCharacters = prompt("Please pick a number between 8-128");
+
+  // the result - the result of the the function: every time a criteria is met it will be added to this var
   var result = [];
   if (numberOfCharacters >= 8 && numberOfCharacters <= 128) {
+    // confirm or deny lower and upper case letter - if both are denied the user will be asked to selecet at least one
     var lowercaseCharacters = confirm("lowercase characters");
     var uppercaseCharacters = confirm("uppercase characters");
     if (!lowercaseCharacters && !uppercaseCharacters) {
-      alert("Please choose at least one something");
+      alert("Please choose at least one type of leters :)");
       lowercaseCharacters = confirm("lowercase characters");
       uppercaseCharacters = confirm("uppercase characters");
     }
+    // confirm of deny special and numeric characters
     var specialCharacters = confirm("Click OK to confirm special chcracters");
     var numericCharacters = confirm("numbers in the password");
 
+    //ransomizing the characters if they are chosen
     var randomLetter = letters[Math.floor(Math.random() * letters.length)];
     var randomUpper =
       uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
@@ -95,7 +102,7 @@ function generatePassword() {
     if (numericCharacters) {
       result.push(randomNumbers);
     }
-
+    // looping the results acording to the number chosen by the user
     for (var i = result.length; i < numberOfCharacters; i++) {
       var randomLetter = letters[Math.floor(Math.random() * letters.length)];
       var randomUpper =
