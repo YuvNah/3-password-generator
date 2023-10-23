d7413ca26be4366a58a067330876746a4e9815ef
Yuval Nahmias <yuvalit789@gmail.com>
2023-10-23 18:37:38 -0400

    finished working password generator: working loop and randomized letters and chcracter

--- assets/script.js
+++ assets/script.js
@@ -29,58 +29,97 @@ var letters = [
   "z",
 ];
 
+var uppercaseLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+console.log;
+
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var randomLetter = letters[Math.floor(Math.random() * letters.length)];
-var randomUpper = randomLetter.toUpperCase();
-var randomSpecial = special[Math.floor(Math.random() * special.length)];
-var randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
-
 function generatePassword() {
-  // create a var of the number chatacters in the password
   var numberOfCharacters = prompt("Please pick a number between 8-128");
-  //var result = [];
+  var result = [];
   if (numberOfCharacters >= 8 && numberOfCharacters <= 128) {
-    //add confirm special characters here
-    //var specialCharacters = confirm("Click OK to confirm special chcracters");
     var lowercaseCharacters = confirm("lowercase characters");
     var uppercaseCharacters = confirm("uppercase characters");
-    //var numericCharacters = confirm("numbers in the password");*/
-    for (var i = 1; i <= numberOfCharacters; i++) {
-      if (uppercaseCharacters) {
-        console.log(randomUpper);
-      }
-      if (lowercaseCharacters) {
-        console.log(randomLetter);
-      }
+    if (!lowercaseCharacters && !uppercaseCharacters) {
+      alert("Please choose at least one something");
+      lowercaseCharacters = confirm("lowercase characters");
+      uppercaseCharacters = confirm("uppercase characters");
     }
-    /*if (specialCharacters) {
-      result.push(randomSpecial);
-    }
+    var specialCharacters = confirm("Click OK to confirm special chcracters");
+    var numericCharacters = confirm("numbers in the password");
+
+    var randomLetter = letters[Math.floor(Math.random() * letters.length)];
+    var randomUpper =
+      uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
+    var randomSpecial = special[Math.floor(Math.random() * special.length)];
+    var randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
 
     if (lowercaseCharacters) {
       result.push(randomLetter);
     }
-
     if (uppercaseCharacters) {
       result.push(randomUpper);
     }
-
+    if (specialCharacters) {
+      result.push(randomSpecial);
+    }
     if (numericCharacters) {
       result.push(randomNumbers);
     }
 
-    if (uppercaseCharacters) {
-      result.push(randomUpper);
-    }*/
+    for (var i = result.length; i < numberOfCharacters; i++) {
+      var randomLetter = letters[Math.floor(Math.random() * letters.length)];
+      var randomUpper =
+        uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
+      var randomSpecial = special[Math.floor(Math.random() * special.length)];
+      var randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+
+      if (lowercaseCharacters) {
+        result.push(randomLetter);
+      } else if (uppercaseCharacters) {
+        result.push(randomUpper);
+      } else if (specialCharacters) {
+        result.push(randomSpecial);
+      } else {
+        result.push(randomNumbers);
+      }
+    }
   } else if (numberOfCharacters < 8) {
     alert("Password length must be at least 8 chcracters");
   } else if (numberOfCharacters > 128) {
     alert("Password lenth cannot be longer than 128 characters");
   }
 
-  //return result.join("");
+  return result.join("");
 }
 
 // Write password to the #password input
